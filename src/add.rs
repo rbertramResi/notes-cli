9a34ebf3351124ccae59dc572ec24429b7d58997
Rob Bertram <=>
2022-02-12 23:43:48 -0600

    add text and title flags

--- src/add.rs
+++ src/add.rs
@@ -1,11 +1,36 @@
 use super::utils;
-use std::fs;
 use std::fs::File;
 use std::io;
 use std::io::Write;
+use gflags::custom::{Arg, Value};
 
-pub fn add_entry(file_name: &str, text: &str) -> Result<(), utils::Error> {
-    let file_name_with_path = format!("{}/{}", utils::get_base_path(), file_name);
+gflags::define! {
+    -t, --title: FlagText
+}
+
+gflags::define! {
+    -t, --text: FlagText
+}
+
+#[derive(Debug)]
+struct FlagText {
+    value: String
+}
+
+impl Value for FlagText {
+    fn parse(arg: Arg) -> gflags::custom::Result<Self> {
+        match arg {
+            s => Ok(FlagText { value: String::from(s.get_str())}),
+        }
+    }
+}
+
+pub fn add_entry() -> Result<(), utils::Error> {
+    gflags::parse();
+    let title = &TITLE.flag.value;
+    let text = &TEXT.flag.value;
+    println!("{:?}", title);
+    let file_name_with_path = format!("{}/{}", utils::get_base_path(), title);
     match File::create(&file_name_with_path) {
         Ok(mut f) => {
             f.write_all(text.as_bytes())?;
